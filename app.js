84fe74b99e9e0e89d33dc9f6fc5f0ddc8deff3d3
Nishita Vaddem <nishitavaddem@gmail.com>
2020-12-18 15:31:39 +0530

    Passing data to views

--- app.js
+++ app.js
@@ -3,24 +3,25 @@ const express = require('express')
 // Express app
 const app = express()
 
+app.set('view engine', 'ejs')
+
 // Listen for requests
 app.listen(3000)
 
 app.get('/', (req, res) => {
     // res.send('<p>Home Page</p>')
-    res.sendFile('./views/index.html', { root: __dirname })
+    res.render('index', { title: 'Home' })
 })
 
 app.get('/about', (req, res) => {
-    res.sendFile('./views/about.html', { root: __dirname })
+    res.render('about', { title: 'About' })
 })
 
-// Redirects
-app.get('/about-us', (req, res) => {
-    res.redirect('/about')
+app.get('/blogs/create', (req, res) => {
+    res.render('create', { title: 'Create a new blog' })
 })
 
 // 404 Page
 app.use((req, res) => {
-    res.sendFile('./views/404.html', { root: __dirname })
+    res.status(400).render('404', { title: '404' })
 })
