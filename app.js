cc56f487003cf5a1c69eff607dccfdf664a7dc15
Nishita Vaddem <nishitavaddem@gmail.com>
2020-12-21 13:59:01 +0530

    Adding middleware and static files

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const morgan = require('morgan')
 
 // Express app
 const app = express()
@@ -7,6 +8,11 @@ app.set('view engine', 'ejs')
 
 // Listen for requests
 app.listen(3000)
+
+// Middleware and static files
+app.use(express.static('public'))
+
+app.use(morgan('dev'))
 
 app.get('/', (req, res) => {
     // res.send('<p>Home Page</p>')
