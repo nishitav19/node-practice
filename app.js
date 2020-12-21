904681e80906d87b486dcc5a404773f7dc1dd948
Nishita Vaddem <nishitavaddem@gmail.com>
2020-12-21 11:31:25 +0530

    Adding partials

--- app.js
+++ app.js
@@ -10,7 +10,12 @@ app.listen(3000)
 
 app.get('/', (req, res) => {
     // res.send('<p>Home Page</p>')
-    res.render('index', { title: 'Home' })
+     const blogs = [
+    {title: 'Yoshi finds eggs', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+    {title: 'Mario finds stars', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+    {title: 'How to defeat bowser', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+  ]
+    res.render('index', { title: 'Home', blogs })
 })
 
 app.get('/about', (req, res) => {
