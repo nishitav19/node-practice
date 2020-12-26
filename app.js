6cc04301ea3167fc0529e112146e49a4c891a087
Nishita Vaddem <nishitavaddem@gmail.com>
2020-12-26 13:58:52 +0530

    Handling delete request

--- app.js
+++ app.js
@@ -66,6 +66,28 @@ app.post('/blogs', (req, res) => {
         })
 })
 
+app.get('/blogs/:id', (req, res) => {
+    const id = req.params.id
+    Blog.findById(id)
+        .then(result => {
+            res.render('details', { title: 'Blog Details', blog: result })
+        })
+        .catch(err => {
+            console.log(err)
+        })
+})
+
+app.delete('/blogs/:id', (req, res) => {
+    const id = req.params.id
+    Blog.findByIdAndDelete(id)
+        .then(result => {
+            res.json({ redirect: '/blogs' })
+        })
+        .catch(err => {
+            console.log(err)
+        })
+})
+
 app.get('/blogs/create', (req, res) => {
     res.render('create', { title: 'Create a new blog' })
 })
